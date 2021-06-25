dee7471dc6fdfead8fc1632f68cce44815e833bc
lucia vilar <vp_lucia@hotmail.com>
2021-06-25 22:41:40 +0200

    sorry, now iteration 1 and 2 but not working

--- js/index.js
+++ js/index.js
@@ -24,20 +24,26 @@ function updateSubtotal(product) {
 // where can I access to it?
 function calculateAll() {
 
-
-
-    // code in the following two lines is added just for testing purposes.
-    // it runs when only iteration 1 is completed. at later point, it can be removed.
-    const singleProduct = document.querySelector('.product');
-    updateSubtotal(singleProduct);
-    // end of test
-
-    // ITERATION 2
-    const
-
-    // ITERATION 3
-    //... your code goes here
+    const allProductos = document.queryAll(".product")
+    for (let product of allProducts) {
+        updateSubtotal(product)
+    }
 }
+// code in the following two lines is added just for testing purposes.
+// it runs when only iteration 1 is completed. at later point, it can be removed.
+const singleProduct = document.querySelector('.product');
+updateSubtotal(singleProduct);
+// end of test
+
+// ITERATION 2
+const allProducts = document.queryAll(".product")
+for (let singleproduct of allProducts) {
+    updateSubtotal(singleproduct)
+}
+
+// ITERATION 3
+//... your code goes here
+
 
 // ITERATION 4
 
